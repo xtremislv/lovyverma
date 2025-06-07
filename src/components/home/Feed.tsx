ed69c75153edcf6a080a0bf15f479e6c65cd72d0
Lovy Verma <106698656+xtremislv@users.noreply.github.com>
2025-06-07 20:15:52 +0530

    Update Feed.tsx

--- src/components/home/Feed.tsx
+++ src/components/home/Feed.tsx
@@ -8,7 +8,7 @@ import { cn } from '@/lib/utils'
 
 export default function Feed() {
   const siteUrl = process.env.NEXT_PUBLIC_SITE_URL
-  const feedUrl = `https://tinyurl.com/2rb8efut`
+  const feedUrl = `${siteUrl}/feed`
   const [copied, setCopied] = useState(false)
 
   const handleCopy = async () => {
